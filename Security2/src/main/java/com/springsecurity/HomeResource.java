101e5734cf8ea5704c8f90f416e0146c54646ef1
uddeshrothe <48801713+uddeshrothe@users.noreply.github.com>
2021-05-14 22:01:56 +0530

    Adding Second Assignment

---
+++ Security2/src/main/java/com/springsecurity/HomeResource.java
@@ -0,0 +1,15 @@
+package com.springsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+	
+	@GetMapping("/")
+	public String home() {
+		return ("<h1>Welcome</h1>");
+				
+	}
+
+}
---
+++ Security2/src/main/java/com/springsecurity/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package com.springsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SuppressWarnings("deprecation")
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		auth.inMemoryAuthentication()
+			.withUser("Joe")
+			.password("rambo")
+			.roles("USER");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http  .authorizeRequests()
+			  .anyRequest().authenticated()
+			  .and()
+			  .formLogin()
+			  .loginPage("/login")
+			  .permitAll();
+	}
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+}
